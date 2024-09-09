b15971365abc33ed0105235a5dd6d7bfb4aa9702
pqienso <buckslimyuan@gmail.com>
2024-09-09 22:31:08 +0800

    use streamlit components instead (please)

--- backend/auth.py
+++ backend/auth.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import asyncio
+import streamlit.components.v1 as components
 
 from httpx_oauth.clients.google import GoogleOAuth2
 from httpx_oauth import oauth2
@@ -30,6 +31,15 @@ async def getUserIdAndEmail(client: GoogleOAuth2, token: str):
 
 def displayLoginButton():
     authorization_url = asyncio.run(getAuthUrl(client, REDIRECT_URI))
+    components.html(f"""
+<div style="display: flex; justify-content: left;">
+    <a href="{authorization_url}" target="_self" style="background-color: {'#fff'}; color: {'#000'}; text-decoration: none; text-align: center; font-size: 16px; margin: 4px 2px; cursor: pointer; padding: 8px 12px; border-radius: 4px; display: flex; align-items: center; font-family: 'Roboto', sans-serif; font-weight: 700;">
+        <img src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA" alt="Google logo" style="margin-right: 8px; width: 26px; height: 26px; background-color: white; border: 2px solid white; border-radius: 4px;">
+        Sign in with Google
+    </a>
+</div>
+""")
+    return
     st.write(f"""
 <div style="display: flex; justify-content: left;">
     <a href="{authorization_url}" target="_self" style="background-color: {'#fff'}; color: {'#000'}; text-decoration: none; text-align: center; font-size: 16px; margin: 4px 2px; cursor: pointer; padding: 8px 12px; border-radius: 4px; display: flex; align-items: center;">
