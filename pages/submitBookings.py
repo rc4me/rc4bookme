c2a64c2efd79b4548e13466de9d403fee449f8c4
pqienso <buckslimyuan@gmail.com>
2024-09-11 22:27:04 +0800

    please work

--- pages/submitBookings.py
+++ pages/submitBookings.py
@@ -35,9 +35,7 @@ st.subheader("Submit bookings")
 now = datetime.now(pytz.timezone("Singapore")).replace(minute=0, second=0)
 startDate = st.date_input("### Start date", value=now.date() + timedelta(days=2))
 startTime = st.time_input("### Start time", step=timedelta(hours=0.5), value=now.time())
-endDate = st.date_input(
-    "### End date", value=startDate + timedelta(days=2), min_value=startDate
-)
+endDate = st.date_input("### End date", min_value=startDate)
 endTime = st.time_input(
     "### End time",
     step=timedelta(hours=0.5),
